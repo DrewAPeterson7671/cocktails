c31146d3054d5e7b71cbc78981659757f3cb6270
Drew Peterson <drew.a.peterson@hotmail.com>
2021-07-29 15:12:57 -0700

    customize the user controller, all tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,56 +4,53 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+    render :index
   end
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
+    render :show
   end
 
   # GET /users/new
   def new
     @user = User.new
+    render :new
   end
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+    render :edit
   end
 
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
     end
+
   end
 
   # DELETE /users/1 or /users/1.json
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to users_path
   end
 
   private
